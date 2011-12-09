066f1f42eb29932cf91f67eebbed8e58146f53d2
Roerd Hinrichsen <roerdhh@gmail.com>
2011-12-09 16:03:55 +0100

    Added a test suite for basic sanity checking, to stop bugs like the recently fixed bug one from appearing that easily.

---
+++ cl-rsvg2-test.asd
@@ -0,0 +1,21 @@
+;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10 -*-
+;;
+;; This software is Copyright (c) Sasha Kovar, 2007,
+;; and Rörd Hinrichsen, 2011.
+;; The copyright holders grant you the rights to
+;; distribute and use this software as governed by
+;; the terms of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
+
+(in-package :cl-user)
+
+(asdf:defsystem cl-rsvg2-test
+  :description "Eos tests for cl-rsvg2."
+  :version "0.4"
+  :author "Rörd Hinrichsen <roerdhh@gmail.com>"
+  :licence "LLGPL"
+  :serial t
+  :components ((:static-file "tests/tux.svg")
+               (:file "tests/rsvg-test"))
+  :depends-on (#:asdf #:cffi #:cl-rsvg2 #:eos))
---
+++ tests/rsvg-test.lisp
@@ -0,0 +1,47 @@
+(defpackage #:cl-rsvg2-test
+  (:nicknames #:rsvg2-test)
+  (:use #:cl #:cffi #:rsvg2 #:eos)
+  (:export #:run!))
+
+(in-package #:cl-rsvg2-test)
+
+(def-suite cl-rsvg2)
+
+(in-suite cl-rsvg2)
+
+(defparameter *tux-file*
+  (namestring
+   (asdf:component-pathname
+    (asdf:find-component :cl-rsvg2-test "tests/tux.svg"))))
+
+(test handle-has-sub
+  (is-true (with-handle-from-file (svg *tux-file*)
+             (handle-has-sub svg "#g3")))
+  (is-false (with-handle-from-file (svg *tux-file*)
+              (handle-has-sub svg "#foo"))))
+
+(test handle-get-dimension-values
+  (is (equal '(744 1052 744d0 1052d0)
+             (multiple-value-list
+              (with-handle-from-file (svg *tux-file*)
+                (handle-get-dimension-values svg))))))
+
+(test handle-get-sub-dimension-values
+  (is (equal '(744 1052 744d0 1052d0)
+             (multiple-value-list
+              (with-handle-from-file (svg *tux-file*)
+                (handle-get-sub-dimension-values svg (null-pointer))))))
+  (is (equal '(593 704 593d0 704d0)
+             (multiple-value-list
+              (with-handle-from-file (svg *tux-file*)
+                (handle-get-sub-dimension-values svg "#g3"))))))
+
+(test handle-get-sub-position-values
+  (is (equal '(0 0)
+             (multiple-value-list
+              (with-handle-from-file (svg *tux-file*)
+                (handle-get-sub-position-values svg (null-pointer))))))
+  (is (equal '(75 173)
+             (multiple-value-list
+              (with-handle-from-file (svg *tux-file*)
+                (handle-get-sub-position-values svg "#g3"))))))
