11c7041481093a409d44ca59aacf4b2dda70ff92
Roerd Hinrichsen <roerdhh@gmail.com>
2011-12-10 13:07:44 +0100

    Added a test function for HANDLE-DATA-STREAM, and use pathnames as much as possible in cairo-test.lisp.

--- tests/cairo-test.lisp
+++ tests/cairo-test.lisp
@@ -3,21 +3,39 @@
 
 (defpackage #:cl-rsvg-cairo-test
   (:nicknames #:rsvg-cairo-test)
-  (:use #:cl #:cairo #:cl-rsvg2)
+  (:use #:cl #:cairo #:cl-rsvg2 #:trivial-gray-streams)
   (:export #:test-rsvg))
 
 (in-package #:cl-rsvg-cairo-test)
 
-(defparameter *rsvg-directory*
-  (namestring (asdf:system-source-directory :cl-rsvg2)))
+(defparameter *tests-directory*
+  (merge-pathnames #p"tests/" (asdf:system-source-directory :cl-rsvg2)))
 
 (defun make-test-filepath (filename)
-  (concatenate 'string *rsvg-directory* "tests/" filename))
+  (merge-pathnames filename *tests-directory*))
 
 (defun create-type-context (type out-file width height)
   (case type
     (:pdf (create-pdf-context out-file width height))
     (:svg (create-svg-context out-file width height))))
+
+;;; Replace the call to DRAW-SVG-FILE in DRAW-WITH-AT to a call to this
+;;; to test the HANDLE-DATA-STREAM class.
+(defun draw-svg-file-stream (filespec)
+  "Draw a SVG file on a Cairo surface. Return its width and height."
+  (let ((data))
+    (with-open-file (in filespec :element-type '(unsigned-byte 8))
+      (let ((vec (make-array (file-length in)
+                             :element-type '(unsigned-byte 8))))
+        (read-sequence vec in)
+        (setf data vec)))
+    (with-handle-data-stream (stream)
+      ;; Just calling WRITE-SEQUENCE here doesn't work as expected in
+      ;; SBCL. It results in many calls to STREAM-WRITE-BYTE instead
+      ;; of a single one to STREAM-WRITE-SEQUENCE. No idea why.
+      (stream-write-sequence stream data 0 (length data))
+      (close stream)
+      (draw-svg (handle stream)))))
 
 (defun draw-with-at (filename scale x y)
   (translate x y)
@@ -56,7 +74,7 @@
   (write-at "i'm a little worried, actually." 12d0 300d0 160d0))
 
 (defun test-rsvg (out-filebase type)
-  (let* ((out-file (make-test-filepath out-filebase))
+  (let* ((out-file (namestring (make-test-filepath out-filebase)))
 	 (width 750d0)
 	 (height 300d0))
     (case type
